cf5de4d8b353254d2ebe2569c37713bb8f612426
Lysenko Nikita <nike7cr77@ya.ru>
2025-02-11 11:19:20 +0700

    refactor postgres

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,8 @@
 FROM python:3.12-slim
 
-WORKDIR: /app
+WORKDIR /app
 
-RUN: apt-get update \
+RUN apt-get update \
     && apt-get install -y gcc libpq-dev \
     && apt-get clean \
     && rm -rf /var/lib/apt/list*
--- config/settings.py
+++ config/settings.py
@@ -79,10 +79,10 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.getenv('NAME'),
-        'USER': os.getenv('USER'),
-        'PASSWORD': os.getenv('PASSWORD'),
-        'HOST': os.getenv('HOST'),
+        'NAME': os.getenv('POSTGRES_DB'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': 'db',
         'PORT': os.getenv('PORT')
     }
 }
